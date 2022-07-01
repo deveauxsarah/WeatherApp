3c6d0cfb49de1df3898e66a78e73128494d1384b
deveauxsarah <95212766+deveauxsarah@users.noreply.github.com>
2022-07-01 09:52:45 +0200

    test

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import logo from './logo.svg';
+import logo from './logo_transparent.png';
+import icon from "./src/icons/sun.svg";
 import './App.css';
 
 function App() {
@@ -15,10 +16,32 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
+          
           Learn React
         </a>
       </header>
+      <div id="root">
+        <div className="row">
+        <div className="col s12 m6 push-m3">
+                <div className="weather card blue-grey darken-1">
+                    <div className="card-content white-text">
+                        <span className="card-title">Lyon</span>
+                        <p><img className="App-icon" alt="icon"src={icon}/></p>
+                        <span className="temperature">15°</span>
+                        <div className="wind">Vent 1km/h (360°)</div>
+                    </div>
+                    <div className="card-action">
+                        <a href="#" style="font-weight: bold;">Thursday</a>
+                        <a href="#">Friday</a>
+                        <a href="#">Saturday</a>
+                        <a href="#">Sunday</a>
+                        <a href="#">Monday</a>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
+</div>
   );
 }
 
